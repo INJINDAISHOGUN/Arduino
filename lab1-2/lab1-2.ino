fe64bc42be533c76fc76ecae515a1cf07b7bdfaf
INJINDAISHOGUN <118966450+INJINDAISHOGUN@users.noreply.github.com>
2023-06-19 12:15:01 +0700

    first commit

---
+++ lab1-2/lab1-2.ino
@@ -0,0 +1,12 @@
+void setup() {
+ pinMode(0 , OUTPUT);//red
+ pinMode(4 , OUTPUT);//green
+ pinMode(5 , OUTPUT);//blue
+ analogWrite(0 , 255);//red
+ analogWrite(4 , 255);//green
+ analogWrite(5 , 255);//blue
+
+}
+
+void loop() {
+}

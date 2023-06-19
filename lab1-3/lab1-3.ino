5f4abc5f3bffbfcf8d4de8e51b6773bf696bd516
INJINDAISHOGUN <118966450+INJINDAISHOGUN@users.noreply.github.com>
2023-06-19 12:17:09 +0700

    update

---
+++ lab1-3/lab1-3.ino
@@ -0,0 +1,12 @@
+void setup() {
+ pinMode(0 , OUTPUT);//red
+ pinMode(4 , OUTPUT);//green
+ pinMode(5 , OUTPUT);//blue
+ analogWrite(0 , 0);//red
+ analogWrite(4 , 0);//green
+ analogWrite(5 , 0);//blue
+
+}
+
+void loop() {
+}
